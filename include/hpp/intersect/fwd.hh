f00d5beb2a714093b04e252606ba820de80f2fdf
Akseppal <akseppal@laas.fr>
2016-07-07 14:58:23 +0200

    Initial commit

---
+++ include/hpp/intersect/fwd.hh
@@ -0,0 +1,36 @@
+//
+//// Copyright (c) 2016 CNRS
+//// Authors: Anna Seppala
+////
+//// This file is part of hpp-affordance
+//// hpp-affordance is free software: you can redistribute it
+//// and/or modify it under the terms of the GNU Lesser General Public
+//// License as published by the Free Software Foundation, either version
+//// 3 of the License, or (at your option) any later version.
+////
+//// hpp-affordance is distributed in the hope that it will be
+//// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//// General Lesser Public License for more details.  You should have
+//// received a copy of the GNU Lesser General Public License along with
+//// hpp-affordance  If not, see
+//// <http://www.gnu.org/licenses/>.
+
+
+#ifndef HPP_INTERSECT_FWD_HH
+#define HPP_INTERSECT_FWD_HH
+
+#include <hpp/fcl/fwd.hh>
+#include <hpp/fcl/BVH/BVH_model.h>
+#include <hpp/fcl/math/vec_3f.h>
+
+namespace hpp {
+      namespace intersect {
+          typedef fcl::BVHModel<fcl::OBBRSS> BVHModelOB;
+          typedef boost::shared_ptr<BVHModelOB> BVHModelOB_Ptr_t;
+          typedef boost::shared_ptr<const BVHModelOB> BVHModelOBConst_Ptr_t;
+      } // namespace intersect
+} // namespace hpp
+
+#endif // HPP_INTERSECT_FWD_HH
+          
---
+++ include/hpp/intersect/intersect.hh
@@ -0,0 +1,37 @@
+//
+//// Copyright (c) 2016 CNRS
+//// Authors: Anna Seppala
+////
+//// This file is part of hpp-affordance
+//// hpp-affordance is free software: you can redistribute it
+//// and/or modify it under the terms of the GNU Lesser General Public
+//// License as published by the Free Software Foundation, either version
+//// 3 of the License, or (at your option) any later version.
+////
+//// hpp-affordance is distributed in the hope that it will be
+//// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+//// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//// General Lesser Public License for more details.  You should have
+//// received a copy of the GNU Lesser General Public License along with
+//// hpp-affordance  If not, see
+//// <http://www.gnu.org/licenses/>.
+//
+//
+#ifndef HPP_INTERSECT_INTERSECT_HH
+#define HPP_INTERSECT_INTERSECT_HH
+
+#include <hpp/intersect/fwd.hh>
+
+namespace hpp {
+    namespace intersect {
+    // free functions ?i
+    
+    /// \addtogroup affordance
+    /// \{
+    /// \}
+    
+    } // namespace intersect
+} // namespace hpp
+    
+#endif // HPP_INTERSECT_INTERSECT_HH
+
